77c441775572831667a3c42151c7e379fa2a1293
Carmel Neta <carmelneta@gmail.com>
2016-08-14 19:17:40 +0300

    z

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,19 +9,39 @@ import { MdIconModule  } from '@angular2-material/icon';
 import { MdSidenavModule  } from '@angular2-material/sidenav';
 import { MdToolbarModule  } from '@angular2-material/toolbar';
 
+import { AngularFireModule } from 'angularfire2';
+import { routing, appRoutingProviders } from './shared/app.routing';
+import { SingInComponent } from './sing-in/sing-in.component';
+
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+  
+const firebaseConfig = {
+  apiKey: "AIzaSyCfswrgYqcAVVCwx5WWjwWzko5sMooCgSw",
+  authDomain: "citybugs-7737e.firebaseapp.com",
+  databaseURL: "https://citybugs-7737e.firebaseio.com",
+  storageBucket: "citybugs-7737e.appspot.com",
+}
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SingInComponent, HomeComponent 
   ],
   imports: [
     BrowserModule,
     CommonModule,
     FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
     MdButtonModule,
     MdIconModule,
-    MdToolbarModule
+    MdToolbarModule,
+    
+    routing
   ],
-  providers: [],
+  providers: [
+    appRoutingProviders
+  ],
   entryComponents: [AppComponent],
   bootstrap: [AppComponent]
 })
